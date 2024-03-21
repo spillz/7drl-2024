70718875124330d2a314be91ab7412d992e67967
Damien Moore <damienlmoore@gmail.com>
2024-03-20 23:46:43 -0400

    Build/deployment script update

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+VERSION="0.0.2"
 npm install
 npm run build
 cd dist
-7zz a breach_in_time_0.0.1.zip index.html assets/*
+7zz a breach_in_time-${VERSION}.zip index.html assets/*
 rm -rf ../../spillz.github.io/7drl-2024/
 mkdir ../../spillz.github.io/7drl-2024/
-cp -r breach_in_time_0.0.3.zip ../../spillz.github.io/7drl-2024/
+cp -r * ../../spillz.github.io/7drl-2024/
 cd ..
